dca48576ff0eed9f70fb8934460b930d06c9953c
Akanksha <akankshaom131999@gmail.com>
2022-07-14 09:23:57 +0530

    change

---
+++ java week/src/static_var.java
@@ -0,0 +1,11 @@
+
+public class static_var {
+	static int x=1;
+	public static void main(String[] args) {
+		System.out.println(x);
+		char x='c';
+		System.out.println(x);
+		
+	}
+
+}
---
+++ java week/src/w1.java
@@ -0,0 +1,12 @@
+
+public class w1 {
+	public static void main(String []args) {
+		System.out.println( "min byte:"+ Byte.MIN_VALUE+"\n");
+		System.out.println( "max byte:"+ Byte.MAX_VALUE);
+		String s="hello world";
+		System.out.println(s.indexOf('w'));
+		
+		
+	}
+
+}
